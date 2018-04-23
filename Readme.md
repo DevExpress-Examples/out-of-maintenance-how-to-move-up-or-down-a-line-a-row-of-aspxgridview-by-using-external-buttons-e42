1af84694c45784d24d57469e76a31e42ae314396
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# How to move up or down a line a row of ASPxGridView by using external buttons
+
+
+<p>This example shows how to move an ASPxGridView row by using external “Up” / “Down” buttons.</p><p>When an end-user presses the “Up” or “Down” button, a <a href="http://documentation.devexpress.com/#AspNet/DevExpressWebASPxGridViewASPxGridView_CustomCallbacktopic"><u>CustomCallback</u></a> event fires and the selected ASPxGridView row moves up or down a line respectively.</p><p><strong>See also:</strong><br />
+<a href="https://www.devexpress.com/Support/Center/p/E4582">E4582: How to reorder ASPxGridView rows using buttons or drag-and-drop</a><u><br />
+</u><a href="https://www.devexpress.com/Support/Center/p/E3850">E3850: How to reorder ASPxTreeList sibling nodes, using buttons or drag-and-drop</a></p>
+
+<br/>
+
+
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,84 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Data
+Imports System.Threading
+Imports DevExpress.Web.ASPxGridView
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Private table As DataTable
+
+	Private ReadOnly Property DataTable() As DataTable
+		Get
+			If Session("DataTable") Is Nothing Then
+				InitializeDataTable()
+				Session("DataTable") = table
+			End If
+			table = CType(Session("DataTable"), DataTable)
+			Return table
+		End Get
+	End Property
+
+	Protected Sub Page_Init(ByVal sender As Object, ByVal e As EventArgs)
+		gv.DataSource = DataTable
+	End Sub
+
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+		If (Not IsPostBack) Then
+			gv.DataBind()
+		End If
+	End Sub
+
+	Private Sub InitializeDataTable()
+		table = New DataTable("Table")
+		Dim column As DataColumn
+
+		column = New DataColumn()
+		column.DataType = GetType(Int32)
+		column.ColumnName = "ID"
+		table.Columns.Add(column)
+
+		table.PrimaryKey = New DataColumn() { column }
+
+		column = New DataColumn()
+		column.DataType = GetType(String)
+		column.ColumnName = "Item"
+		table.Columns.Add(column)
+
+		PopulateDataTable()
+	End Sub
+
+	Private Sub PopulateDataTable()
+		Dim row As DataRow
+		For i As Integer = 0 To 19
+			row = table.NewRow()
+			row("ID") = i
+			row("Item") = "Item " & i
+			table.Rows.Add(row)
+		Next i
+	End Sub
+
+	Protected Sub gv_CustomCallback(ByVal sender As Object, ByVal e As ASPxGridViewCustomCallbackEventArgs)
+		Dim gridView As ASPxGridView = CType(sender, ASPxGridView)
+		Dim index As Integer = gridView.FocusedRowIndex
+		If (e.Parameters = "up" AndAlso index <= 0) OrElse (e.Parameters = "down" AndAlso index >= gv.VisibleRowCount - 1) Then
+			Return
+		End If
+		Dim id As Integer = CInt(Fix(gridView.GetRowValues(index, "ID")))
+
+        Dim nextIndex As Integer = If(e.Parameters = "up", index - 1, index + 1)
+
+		Dim nextId As Integer = CInt(Fix(gridView.GetRowValues(nextIndex, "ID")))
+
+		Dim dataRow As DataRow = table.Rows.Find(id)
+		dataRow("ID") = Integer.MaxValue
+
+		Dim nextDataRow As DataRow = table.Rows.Find(nextId)
+		nextDataRow("ID") = id
+
+		dataRow("ID") = nextId
+
+		gridView.DataBind()
+		gridView.FocusedRowIndex = nextIndex
+	End Sub
+End Class
